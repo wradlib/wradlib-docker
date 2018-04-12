83872e168e502151000cfd12c39eca3c98437e46
Kai Mühlbauer <kmuehlbauer@users.noreply.github.com>
2018-04-12 14:43:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,18 @@
 
 # add wradlib user
 USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-9001} 
+USER_NAME=${LOCAL_USER_NAME:-wradlib}
 echo "Container USER_ID : $USER_ID"
-adduser -s /bin/bash -m -u $USER_ID wradlib
+echo "Container GROUP_ID : $GROUP_ID"
+echo "Container USER_NAME : $USER_NAME"
+
+useradd -s /bin/bash -m -u $USER_ID -g $GROUP_ID $USER_NAME
 
 # chown home directory
-export HOME=/home/wradlib
-chown -R wradlib $HOME
-chown -R wradlib $CONDA_DIR
+export HOME=/home/$USER_NAME
+chown -R $USER_NAME $HOME
+chown -R $USER_NAME $CONDA_DIR
 
 # use su-exec to run the process as wradlib
-su-exec wradlib $@
+su-exec $USER_NAME $@
