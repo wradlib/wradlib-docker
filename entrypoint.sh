84c5e8516eed2add10bdfd4a99ae91780fd13d8b
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2020-06-24 09:31:51 +0200

    MNT: style

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,20 +7,21 @@ echo ". ${CONDA_DIR}/etc/profile.d/conda.sh && conda activate wradlib" >> ~/.bas
 USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-100} 
 USER_NAME=${LOCAL_USER_NAME:-wradlib}
-echo "Container USER_ID : $USER_ID"
-echo "Container GROUP_ID : $GROUP_ID"
-echo "Container USER_NAME : $USER_NAME"
+echo "Container USER_ID : ${USER_ID}"
+echo "Container GROUP_ID : ${GROUP_ID}"
+echo "Container USER_NAME : ${USER_NAME}"
 
-useradd -s /bin/bash -m -u $USER_ID -g $GROUP_ID $USER_NAME
+useradd -s /bin/bash -m -u ${USER_ID} -g ${GROUP_ID} ${USER_NAME}
 
 # chown home directory
-export HOME=/home/$USER_NAME
+export HOME=/home/${USER_NAME}
 
-mkdir $HOME/.jupyter
-echo "c.MultiKernelManager.default_kernel_name = 'wradlib'" >> $HOME/.jupyter/jupyter_notebook_config.py
+mkdir ${HOME}/.jupyter
+echo "c.MultiKernelManager.default_kernel_name = 'wradlib'" >> ${HOME}/.jupyter/jupyter_notebook_config.py
 
-chown -R $USER_NAME:$GROUP_ID $HOME
-chown -R $USER_NAME:$GROUP_ID $CONDA_DIR
+chown -R ${USER_NAME}:${GROUP_ID} ${HOME}
+chown -R ${USER_NAME}:${GROUP_ID} ${CONDA_DIR}
 
 # use su-exec to run the process as wradlib
-su-exec $USER_NAME $@
+echo $@
+su-exec ${USER_NAME} $@
